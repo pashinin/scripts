fd1f150f0d970e2af40cf9f236b7a517ef3171e5
Sergey Pashinin <sergey@pashinin.com>
2017-11-01 21:57:01 +0300

    localhost

--- ubuntu-setup/server.sh
+++ ubuntu-setup/server.sh
@@ -4,6 +4,7 @@ which pip || sudo apt-get install -y python-pip
 which ansible || sudo -H pip install ansible -U
 which sshd || sudo apt-get -y install ssh
 
+wget -O /tmp/hosts 'https://raw.githubusercontent.com/pashinin/scripts/master/ubuntu-setup/hosts'
 wget -O /tmp/server.yml 'https://raw.githubusercontent.com/pashinin/scripts/master/ubuntu-setup/server.yml'
-ansible-playbook /tmp/server.yml
+ansible-playbook -i /tmp/hosts /tmp/server.yml
 # ansible-playbook -i hosts desktop_as_user.yml -f 10
